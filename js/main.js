9b2b845521069c00518b580ed98ed65c3084f1ef
Doug Rucker <dougruck@gmail.com>
2015-03-12 21:03:21 -0700

    Fix map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+var sites = {
+    "stps": [
+        {
+            "name": "Bothell-3",
+            "address": "18715 120th Ave NE",
+            "city": "Bothell",
+            "state": "WA",
+            "zip": "98021",
+            "location" : "47.764047,-122.178302"
+        },
+        {
+            "name": "Bothell-9",
+            "address": "20307 N Creek Pkwy",
+            "city": "Bothell",
+            "state": "WA",
+            "zip": "98011",
+            "location" : "47.774568,-122.188956"
+        },
+        {
+            "name": "Sacramento",
+            "address": "10000 Goethe Way",
+            "city": "Sacramento",
+            "state": "WA",
+            "zip": "95827",
+            "location" : "38.552673,-121.329446"
+        }
+    ]
+}
+
+
+
+
+
+
 var styles = [
   {
     stylers: [
@@ -39,12 +73,26 @@ $(window).on('resize', function() {
         var map = new google.maps.Map(mapCanvas, mapOptions)
         map.setOptions({styles: styles});
 
-var myLatlng = new google.maps.LatLng(47.717951, -122.309879);
+//var temp = sites.stps[1].location.split(",");
+//console.log(temp[0],",",temp[1]);
+//var myLatlng = new google.maps.LatLng(temp[0],temp[1]);
 
+for (var node in sites.stps) {
+  console.log(sites.stps[node].location);
+  var temp = sites.stps[node].location.split(",");
+  var myLatlng = new google.maps.LatLng(temp[0],temp[1]);
   var marker = new google.maps.Marker({
       position: (myLatlng),
       map: map,
-      title: 'Hello World!'
+      title: sites.stps[node].name,
   });
+
+}
+
+  /*var marker = new google.maps.Marker({
+      position: (myLatlng),
+      map: map,
+      title: 'Hello World!'
+  });*/
   }
       google.maps.event.addDomListener(window, 'load', initialize);
